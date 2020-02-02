e258dadb7c4bdf1ab1f2dec2f4f93273a3908e38
Rob Brennan <rob@therobbrennan.com>
2020-02-01 17:24:21 -0800

    Moved STRIPE_API_KEY_TEST to be a const we can export

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -5,8 +5,9 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+export const STRIPE_API_KEY_TEST = "pk_test_OYSwXOmUEJLnvMkoCktnbESY00hR7YrcUs"
+
 const IndexPage = () => {
-  const STRIPE_API_KEY_TEST = "pk_test_OYSwXOmUEJLnvMkoCktnbESY00hR7YrcUs"
   // REMEMBER: window is not available in server side rendering (SSR)
   const stripe = (typeof window !== `undefined`) ? window.Stripe(STRIPE_API_KEY_TEST) : undefined
   try {
