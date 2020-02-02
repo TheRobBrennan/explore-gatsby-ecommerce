6b07ee5b80acf3800e3ee02ac7045652f462ded2
Rob Brennan <rob@therobbrennan.com>
2020-02-01 19:09:18 -0800

    Product now displays a specific product and skus

--- app/src/components/product.js
+++ app/src/components/product.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react"
-import Layout from "../components/layout"
 
-export const STRIPE_API_KEY_TEST = "pk_test_OYSwXOmUEJLnvMkoCktnbESY00hR7YrcUs"
+export const STRIPE_API_KEY_TEST = "pk_test_OYSwXOmUEJLnvMkoCktnbESY00hR7YrcUs" // Publishable key from the Stripe Dashboard > Developers > API keys
 export const STRIPE_REDIRECT_URL_SUCCESS = "http://localhost:8000/success"
 export const STRIPE_REDIRECT_URL_CANCEL = "http://localhost:8000/cancel"
 
-// TODO: Product is hard-coded to our example T-shirt.
-const Product = () => {
-  const [sku, setSku] = useState("")
+const Product = ({ skus, product }) => {
+  const DEFAULT_SKU = skus[0].node.id || ""
+  const [sku, setSku] = useState(DEFAULT_SKU)
 
   const placeOrder = () => {
     try {
@@ -33,24 +32,28 @@ const Product = () => {
   }
 
   return (
-    <Layout>
-      <div>
-        <article>
-          <img
-            src='https://picsum.photos/340/400'
-            alt="A sample t-shirt that doesn't exist and will never ship."
-          />
-          <h3>Sample T-Shirt</h3>
+    <article>
+      {/* TODO: Replace the placeholder image with a product image */}
+      <img
+        src='https://picsum.photos/340/400'
+        alt="A sample item that doesn't exist and will never ship."
+      />
+      <h3>{product.name}</h3>
 
-          <select defaultValue={sku} onChange={e => setSku(e.target.value)}>
-            <option value='sku_GerD9TIXnWbbqc'>Small</option>
-            <option value='sku_GerDdIN6LB5hdR'>Medium</option>
-            <option value='sku_GerDERk6FoMzg3'>Large</option>
-          </select>
-          <button onClick={() => placeOrder()}>Buy Me</button>
-        </article>
-      </div>
-    </Layout>
+      <select defaultValue={sku} onChange={e => setSku(e.target.value)}>
+        {skus.map(edge => {
+          const skuFromEdge = edge.node.id
+          const nameForSkuFromEdge = edge.node.attributes.name
+
+          return (
+            <option key={skuFromEdge} value={skuFromEdge}>
+              {nameForSkuFromEdge}
+            </option>
+          )
+        })}
+      </select>
+      <button onClick={() => placeOrder()}>Buy Me</button>
+    </article>
   )
 }
 
